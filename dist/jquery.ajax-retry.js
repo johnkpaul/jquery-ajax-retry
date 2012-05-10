1f0f20a5b786e1e372824c219a5e86f79790265b
John Paul <john@johnkpaul.com>
2012-05-10 15:46:21 -0400

    add dist generated files

---
+++ dist/jquery.ajax-retry.js
@@ -0,0 +1,55 @@
+/*! jQuery Ajax Retry - v0.1.0 - 2012-05-10
+* https://github.com/johnkpaul/jquery-ajax-retry
+* Copyright (c) 2012 John Paul; Licensed MIT */
+
+(function($) {
+
+  $(function(){
+    var tempAjax = $.ajax;
+
+    $.ajax = function(){
+      var deferred = tempAjax.apply({},arguments);
+      deferred.retry = passThroughToPipe;
+      deferred.withTimeout = function(timeout){
+        this.timeout = timeout;
+        return this;
+      };
+      return deferred;
+    };
+
+
+    function passThroughToPipe(times){
+        return this.pipe(null, retryIt.call(this,times));
+    }
+
+    function retryIt(times){
+      var self = this;
+      return function(deferred,status,msg){
+        var ajaxOptions = this; 
+        var def = new $.Deferred();
+
+        if(self.timeout !== undefined){
+          var timeoutDeferred = new $.Deferred();
+
+          $.when(timeoutDeferred).then(function(timeout){
+            def.pipe($.ajax(ajaxOptions).pipe(null, retryIt.call(self,times - 1)));
+          });
+
+          setTimeout(function(){
+            timeoutDeferred.resolve();
+          }, this.timeout);
+
+          return def;
+        }
+
+        if(times > 1){
+          return $.ajax(this).pipe(null, retryIt(times - 1));
+        }     
+
+        def.rejectWith(this,arguments);
+        return def;
+      };  
+    }
+  });
+
+}(jQuery));
