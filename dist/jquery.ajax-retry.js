678fe38e4a852eac8359434677e5f7a16b07b565
Corey Frang <gnarf@gnarf.net>
2012-09-16 06:40:56 -0500

    Removing pointless document ready, and using ajaxPrefilter instead of duck punching ajax

--- dist/jquery.ajax-retry.js
+++ dist/jquery.ajax-retry.js
@@ -1,55 +1,49 @@
-/*! jQuery Ajax Retry - v0.1.0 - 2012-05-10
+/*! jQuery Ajax Retry - v0.1.0 - 2012-09-16
 * https://github.com/johnkpaul/jquery-ajax-retry
 * Copyright (c) 2012 John Paul; Licensed MIT */
 
 (function($) {
 
-  $(function(){
-    var tempAjax = $.ajax;
-
-    $.ajax = function(){
-      var deferred = tempAjax.apply({},arguments);
-      deferred.retry = passThroughToPipe;
-      deferred.withTimeout = function(timeout){
-        this.timeout = timeout;
-        return this;
-      };
-      return deferred;
+  $.ajaxPrefilter(function(options, originalOptions, jqXHR){
+    jqXHR.retry = passThroughToPipe;
+    jqXHR.withTimeout = function(timeout){
+      this.timeout = timeout;
+      return this;
     };
-
-
-    function passThroughToPipe(times){
-        return this.pipe(null, retryIt.call(this,times));
-    }
-
-    function retryIt(times){
-      var self = this;
-      return function(deferred,status,msg){
-        var ajaxOptions = this; 
-        var def = new $.Deferred();
-
-        if(self.timeout !== undefined){
-          var timeoutDeferred = new $.Deferred();
-
-          $.when(timeoutDeferred).then(function(timeout){
-            def.pipe($.ajax(ajaxOptions).pipe(null, retryIt.call(self,times - 1)));
-          });
-
-          setTimeout(function(){
-            timeoutDeferred.resolve();
-          }, this.timeout);
-
-          return def;
-        }
-
-        if(times > 1){
-          return $.ajax(this).pipe(null, retryIt(times - 1));
-        }     
-
-        def.rejectWith(this,arguments);
-        return def;
-      };  
-    }
   });
+
+
+  function passThroughToPipe(times){
+      return this.pipe(null, retryIt.call(this,times));
+  }
+
+  function retryIt(times){
+    var self = this;
+    return function(deferred,status,msg){
+      var ajaxOptions = this; 
+      var def = new $.Deferred();
+
+      if(self.timeout !== undefined){
+        var timeoutDeferred = new $.Deferred();
+
+        $.when(timeoutDeferred).then(function(timeout){
+          def.pipe($.ajax(ajaxOptions).pipe(null, retryIt.call(self,times - 1)));
+        });
+
+        setTimeout(function(){
+          timeoutDeferred.resolve();
+        }, self.timeout);
+
+        return def;
+      }
+
+      if(times > 1){
+        return $.ajax(this).pipe(null, retryIt(times - 1));
+      }     
+
+      def.rejectWith(this,arguments);
+      return def;
+    };  
+  }
 
 }(jQuery));
