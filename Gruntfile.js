6861a01a8b8a4fa3407e3b470d558304ff2bd2a0
John K. Paul <john@johnkpaul.com>
2014-06-14 17:06:11 -0400

    Update to grunt 0.4

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,28 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
+  require('load-grunt-tasks')(grunt);
+
   // Project configuration.
   grunt.initConfig({
-    pkg: '<json:package.json>',
-    meta: {
-      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-        '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
-        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
-    },
+    pkg: grunt.file.readJSON('package.json'),
     concat: {
       dist: {
-        src: ['<banner:meta.banner>', '<file_strip_banner:src/<%= pkg.name %>.js>'],
+        src: ['src/<%= pkg.name %>.js'],
         dest: 'dist/<%= pkg.name %>.js'
       }
     },
-    min: {
+    uglify: {
       dist: {
-        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
+        src: ['<%= concat.dist.dest %>'],
         dest: 'dist/<%= pkg.name %>.min.js'
       }
     },
     qunit: {
       files: ['test/**/*.html']
     },
-    lint: {
-      files: ['grunt.js', 'src/**/*.js', 'test/**/*.js']
-    },
-    watch: {
-      files: '<config:lint.files>',
-      tasks: 'lint qunit'
-    },
     jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
       options: {
         curly: true,
         eqeqeq: true,
@@ -45,19 +34,19 @@ module.exports = function(grunt) {
         undef: true,
         boss: true,
         eqnull: true,
-        browser: true
-      },
-      globals: {
-        jQuery: true,
-        define: true,
-        require: true
+        browser: true,
+        globals: {
+          jQuery: true,
+          define: true,
+          ok: true,
+          require: true
+        }
       }
-    },
-    uglify: {}
+    }
   });
 
   // Default task.
-  grunt.registerTask('default', 'lint qunit concat min manifest');
+  grunt.registerTask('default', ['jshint','qunit', 'concat', 'uglify', 'manifest']);
 
   grunt.registerTask( "manifest", function() {
     var pkg = grunt.config( "pkg" );
@@ -81,4 +70,5 @@ module.exports = function(grunt) {
       }
     }, null, "  " ) );
   });
+  
 };
